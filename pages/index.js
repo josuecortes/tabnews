cc1b5a12acd4086c0b8d9282c0adfe7054a76346
Josue Cortes <josue.cort3s@gmail.com>
2025-02-07 22:53:44 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import { lazy } from "react";
+
 function Home() {
-  return <h1>Amor, eu to fazendo esse curso aqui pensando em você e na nossa familia. TE AMO</h1>
+  return (
+    <h1>
+      Amor, eu to fazendo esse curso aqui pensando em você e na nossa familia.
+      TE AMO
+    </h1>
+  );
 }
 
 export default Home;
